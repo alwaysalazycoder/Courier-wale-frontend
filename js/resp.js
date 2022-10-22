59745402c5c6755905422e401b140f56f244eaba
alwaysalazycoder <gs314315316@gmail.com>
2022-10-23 04:10:36 +0530

    commit to main

---
+++ js/resp.js
@@ -0,0 +1,11 @@
+let resp = document.getElementById("responsive-menu-bar");
+let responsiveClass = document.getElementById("responsive-nav-class");
+
+
+resp.addEventListener('click', () => {
+    if (responsiveClass.style.visibility === "inherit") {
+        responsiveClass.style.visibility = "hidden";
+    } else {
+        responsiveClass.style.visibility = "inherit";
+    }
+})
